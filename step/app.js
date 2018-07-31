b1593b9e0eb5d38be8092018a8a2e44bb69668dc
seyoon.kim <ksy7533@zuminternet.com>
2018-07-31 14:35:16 +0900

    사용자로 부터 입력받은 값을 리스트에 추가하기 1. 입력 버튼 클릭과 엔터를 눌렀을 경우 이벤트 핸들러 처리하기 2. 이벤트 핸들러에서는 사용자로 부터 입력받은값 저장하기 3. 입력받은 값을 HTML 리스트 형태로 만들어서 붙이기

--- step/app.js
+++ step/app.js
@@ -2,4 +2,16 @@ var budgetController = (function() {})();
 
 var UIController = (function() {})();
 
-var controller = (function(budgetCtrl, UICtrl) {})(budgetController, UIController)
+var controller = (function(budgetCtrl, UICtrl) {
+    function addListItem() {
+        var type = document.querySelector('.add__type').value;
+        var description = document.querySelector('.add__description').value;
+        var value = document.querySelector('.add__value').value;
+        var html = '<div class="item clearfix" id="income-0"><div class="item__description">{{description}}</div><div class="right clearfix"><div class="item__value">{{value}}</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+        var rHtml = html.replace('{{description}}', description);
+        rHtml = rHtml.replace('{{value}}', value);
+        document.querySelector('.income__list').insertAdjacentHTML('beforeend', rHtml);
+    };
+
+    document.querySelector('.add__btn').addEventListener('click', addListItem);
+})(budgetController, UIController)
