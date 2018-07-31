dbc958c8f70dc2c3cd53602ec3a641b0114d25ce
seyoon.kim <ksy7533@zuminternet.com>
2018-07-31 15:56:57 +0900

    DOMStrings 생성, 엘리멘트 상수 모음

--- step/app.js
+++ step/app.js
@@ -5,9 +5,21 @@ var UIController = (function() {
     var totalIncome = 0;
     var totalExpense = 0;
 
+    var DOMStrings = {
+        ADD_TYPE: '.add__type',
+        ADD_DESCRIPTION: '.add__description',
+        ADD_VALUE: '.add__value',
+        ADD_BTN: '.add__btn',
+        BUDGET_INCOME_VALUE: '.budget__income--value',
+        BUDGET_EXPENSES_VALUE: '.budget__expenses--value',
+        BUDGET_VALUE: '.budget__value',
+        INCOME_LIST: '.income__list',
+        EXPENSE_LIST: '.expenses__list'
+    }
+
     var updateTotalBudget = function() {
-        var type = document.querySelector('.add__type').value;
-        var value = parseInt(document.querySelector('.add__value').value);
+        var type = document.querySelector(DOMStrings.ADD_TYPE).value;
+        var value = parseInt(document.querySelector(DOMStrings.ADD_VALUE).value);
 
         if (type === 'income') {
             totalIncome += value;
@@ -17,9 +29,9 @@ var UIController = (function() {
 
         totalBudget = totalIncome - totalExpense;
 
-        var incomeValue = document.querySelector('.budget__income--value');
-        var expenseValue = document.querySelector('.budget__expenses--value');
-        var budgetValue = document.querySelector('.budget__value');
+        var incomeValue = document.querySelector(DOMStrings.BUDGET_INCOME_VALUE);
+        var expenseValue = document.querySelector(DOMStrings.BUDGET_EXPENSES_VALUE);
+        var budgetValue = document.querySelector(DOMStrings.BUDGET_VALUE);
 
         incomeValue.textContent = totalIncome;
         expenseValue.textContent = totalExpense;
@@ -27,9 +39,9 @@ var UIController = (function() {
     }
 
     var addListItem = function() {
-        var type = document.querySelector('.add__type').value;
-        var description = document.querySelector('.add__description').value;
-        var value = document.querySelector('.add__value').value;
+        var type = document.querySelector(DOMStrings.ADD_TYPE).value;
+        var description = document.querySelector(DOMStrings.ADD_DESCRIPTION).value;
+        var value = document.querySelector(DOMStrings.ADD_VALUE).value;
 
         if (value === '' || description === '') {
             return;
@@ -38,10 +50,10 @@ var UIController = (function() {
         var html, rHtml, list;
         if (type === 'income') {
             html = '<div class="item clearfix" id="income-0"><div class="item__description">{{description}}</div><div class="right clearfix"><div class="item__value">{{value}}</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
-            list = document.querySelector('.income__list');
+            list = document.querySelector(DOMStrings.INCOME_LIST);
         } else if (type === 'expense') {
             html = '<div class="item clearfix" id="expense-0"><div class="item__description">{{description}}</div><div class="right clearfix"><div class="item__value">{{value}}</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
-            list = document.querySelector('.expenses__list');
+            list = document.querySelector(DOMStrings.EXPENSE_LIST);
         }
         rHtml = html.replace('{{description}}', description);
         rHtml = rHtml.replace('{{value}}', value);
@@ -50,7 +62,8 @@ var UIController = (function() {
 
     return {
         updateTotalBudget: updateTotalBudget,
-        addListItem: addListItem
+        addListItem: addListItem,
+        DOMStrings: DOMStrings
     }
 
 })();
@@ -61,7 +74,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         UICtrl.updateTotalBudget();
     }
 
-    document.querySelector('.add__btn').addEventListener('click', updateList);
+    document.querySelector(UICtrl.DOMStrings.ADD_BTN).addEventListener('click', updateList);
     document.addEventListener('keypress', function(event) {
         if (event.keyCode !== 13) {
             return;
