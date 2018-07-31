d0b518a1367531aa824cc3ca29ae5afb36060c6d
seyoon.kim <ksy7533@zuminternet.com>
2018-07-31 14:42:04 +0900

    type이 income인 경우와 expense인 경우 html이 다르게 생성되도록 한다.

--- step/app.js
+++ step/app.js
@@ -7,10 +7,21 @@ var controller = (function(budgetCtrl, UICtrl) {
         var type = document.querySelector('.add__type').value;
         var description = document.querySelector('.add__description').value;
         var value = document.querySelector('.add__value').value;
-        var html = '<div class="item clearfix" id="income-0"><div class="item__description">{{description}}</div><div class="right clearfix"><div class="item__value">{{value}}</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
-        var rHtml = html.replace('{{description}}', description);
+        var html, rHtml, list;
+        if (type === 'income') {
+            html = '<div class="item clearfix" id="income-0"><div class="item__description">{{description}}</div><div class="right clearfix"><div class="item__value">{{value}}</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+            list = document.querySelector('.income__list');
+        } else if (type === 'expense') {
+            html = '<div class="item clearfix" id="expense-0"><div class="item__description">{{description}}</div><div class="right clearfix"><div class="item__value">{{value}}</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+            list = document.querySelector('.expenses__list');
+        }
+        rHtml = html.replace('{{description}}', description);
         rHtml = rHtml.replace('{{value}}', value);
-        document.querySelector('.income__list').insertAdjacentHTML('beforeend', rHtml);
+        list.insertAdjacentHTML('beforeend', rHtml);
+
+        /*
+            type이 income인 경우와 expense인 경우 html이 다르게 생성되도록 한다.
+        */
     };
 
     document.querySelector('.add__btn').addEventListener('click', addListItem);
